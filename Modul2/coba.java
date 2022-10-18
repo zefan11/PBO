6eedb4d722efe22ec75a3e6785507b5902a30e4a
Zefanya Andin Novrean <89759269+zefan11@users.noreply.github.com>
2022-10-18 12:24:07 +0700

    Create coba.java

---
+++ Modul2/coba.java
@@ -0,0 +1,13 @@
+public class Modul02 {
+    public static void main(String[] args) {
+        //Persegi Panjang
+        PersegiPanjang p1 = new PersegiPanjang(17,8);
+        p1.hitungKeliling();
+   
+       PersegiPanjang p2 = new PersegiPanjang(26,14);
+       p2.hitungKeliling();
+        //Lingkaran
+       Lingkaran L1 = new Lingkaran(6);
+       Lingkaran L2 = new Lingkaran(12);
+    }
+}
